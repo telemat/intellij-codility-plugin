d21b3ab1c20dc0188c827c27026634e57dd27734
GitHub Action <action@github.com>
2020-08-16 07:29:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/telemat/intellijcodilityplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.telemat.intellijcodilityplugin.services
+
+import com.github.telemat.intellijcodilityplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/telemat/intellijcodilityplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/telemat/intellijcodilityplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.telemat.intellijcodilityplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.telemat.intellijcodilityplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
